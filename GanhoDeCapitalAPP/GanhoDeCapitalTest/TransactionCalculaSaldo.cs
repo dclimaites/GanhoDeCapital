c5d7f57727bf24a878c1fc2436eea5cec2694b3f
Diego Climaites <diegoclimaites@gmail.com>
2022-03-31 10:17:18 -0300

    Adicionado método para testar quando a transação da prejuízo

--- GanhoDeCapitalAPP/GanhoDeCapitalTest/TransactionCalculaSaldo.cs
+++ GanhoDeCapitalAPP/GanhoDeCapitalTest/TransactionCalculaSaldo.cs
@@ -10,22 +10,43 @@ namespace GanhoDeCapitalTest
         {
             yield return new object[] {
                 new Transaction("sell", 20m, 5000, 10m)
-                , new List<Transaction>()
-                {
-                    new Transaction("buy", 10m, 10000, 10m),
-                    new Transaction("sell", 20m, 5000, 10m)
-                }
+                //, new List<Transaction>()
+                //{
+                //    new Transaction("buy", 10m, 10000, 10m),
+                //    new Transaction("sell", 20m, 5000, 10m)
+                //}
                 ,50000m
 
             };
         }
 
+        public static IEnumerable<object[]> TransacoesSemLucro()
+        {
+            yield return new object[] {
+                new Transaction("sell", 9m, 5000, 10m)
+                ,-5000m
+            };
+        }
+
         [Theory]
         [MemberData(nameof(TransacoesComLucro))]
-        public void CalculaPrecoMedioDadoTransacoesComCompra(Transaction transacaoSobAnalise, IEnumerable<Transaction> transacoes, decimal valorEsperado)
+        public void CalculaSaldoDadoTransacaoComLucro(Transaction transacaoSobAnalise, decimal valorEsperado)
         {
             //arrange
-            var trade = new Trade(transacoes);
+            //var trade = new Trade(transacoes);
+            //act
+            var saldo = transacaoSobAnalise.Balance();
+            //assert
+            Assert.Equal(valorEsperado, saldo);
+
+        }
+
+        [Theory]
+        [MemberData(nameof(TransacoesComLucro))]
+        public void CalculaSaldoDadoTransacaoSemLucro(Transaction transacaoSobAnalise, decimal valorEsperado)
+        {
+            //arrange
+            //var trade = new Trade(transacoes);
             //act
             var saldo = transacaoSobAnalise.Balance();
             //assert
