33239a8b80f644889ef6ce8f7d505d1c335fb2b1
Diego Climaites <diegoclimaites@gmail.com>
2022-03-31 11:15:19 -0300

    Adicionado método de teste para cálculo de imposto com prejuízo acumulado

--- GanhoDeCapitalAPP/GanhoDeCapitalAPP/Domain/Tax.cs
+++ GanhoDeCapitalAPP/GanhoDeCapitalAPP/Domain/Tax.cs
@@ -7,11 +7,13 @@ namespace GanhoDeCapitalAPP.Domain
     {
         public Transaction Transaction { get; }
         public decimal AveragePrice { get; }
+        public decimal Losses { get; }
         public decimal TaxRate { get; }
-        public Tax(Transaction transaction, decimal averagePrice)
+        public Tax(Transaction transaction, decimal averagePrice, decimal losses)
         {
             Transaction = transaction;
             AveragePrice = averagePrice;
+            Losses = losses;
             TaxRate = 0.2m;
         }
 
@@ -19,7 +21,10 @@ namespace GanhoDeCapitalAPP.Domain
         {
             if (HasTax())
             {
-                return TaxRate * Transaction.Balance();
+                var currentBalance = Transaction.Balance();
+                var balanceWithoutLosses = currentBalance - Losses;
+                if(balanceWithoutLosses > 0)
+                    return balanceWithoutLosses * TaxRate;
             }
 
             return 0m;
--- GanhoDeCapitalAPP/GanhoDeCapitalTest/TaxCalculaImposto.cs
+++ GanhoDeCapitalAPP/GanhoDeCapitalTest/TaxCalculaImposto.cs
@@ -36,18 +36,84 @@ namespace GanhoDeCapitalTest
             };
         }
 
+        public static IEnumerable<object[]> TransacoesComLucroEPrejuizoAcumulado()
+        {
+            yield return new object[] {
+                new Transaction("sell", 12m, 100, 10m)
+                , 10m
+                , 2000m
+                , 0m
+
+            };
+        }
+
+        public static IEnumerable<object[]> TransacoesSemLucroEPrejuizoAcumulado()
+        {
+            yield return new object[] {
+                new Transaction("sell", 9m, 100, 10m)
+                , 10m
+                , 2000m
+                , 0
+            };
+        }
+        public static IEnumerable<object[]> TransacoesComLucroInferiroAoPrejuizoAcumulado()
+        {
+            yield return new object[] {
+                new Transaction("sell", 15m, 100, 10m)
+                , 10m
+                , 2000m
+                , 0
+            };
+        }
+
+        public static IEnumerable<object[]> TransacoesComLucroIgualAoPrejuizoAcumulado()
+        {
+            yield return new object[] {
+                new Transaction("sell", 15m, 400, 10m)
+                , 10m
+                , 2000m
+                , 0
+            };
+        }
+
+        public static IEnumerable<object[]> TransacoesComLucroSuperiorAoPrejuizoAcumulado()
+        {
+            yield return new object[] {
+                new Transaction("sell", 15m, 5000, 10m)
+                , 10m
+                , 20000m
+                , 1000m
+            };
+        }
+
         [Theory]
         [MemberData(nameof(TransacoesComLucro))]
         [MemberData(nameof(TransacoesSemLucro))]
         public void CalculaImpostoDadoTransacaoComLucro(Transaction transacaoSobAnalise, decimal precoMedio, decimal valorEsperado)
         {
             //arrange
-            var regra = new Tax(transacaoSobAnalise, precoMedio);
+            var regra = new Tax(transacaoSobAnalise, precoMedio, 0);
             //act
             var imposto = regra.CalculateTax();
             //assert
             Assert.Equal(valorEsperado, imposto);
 
+        }
+
+        [Theory]
+        [MemberData(nameof(TransacoesComLucroEPrejuizoAcumulado))]
+        [MemberData(nameof(TransacoesSemLucroEPrejuizoAcumulado))]
+        [MemberData(nameof(TransacoesComLucroInferiroAoPrejuizoAcumulado))]
+        [MemberData(nameof(TransacoesComLucroIgualAoPrejuizoAcumulado))]
+        [MemberData(nameof(TransacoesComLucroSuperiorAoPrejuizoAcumulado))]
+        public void CalculaImpostoDadoTransacaoComPrejuizoAcumulado(Transaction transacaoSobAnalise, decimal precoMedio, decimal prejuizoAcumulado, decimal valorEsperado)
+        {
+            //arrange
+            var regra = new Tax(transacaoSobAnalise, precoMedio, prejuizoAcumulado);
+            //act
+            var imposto = regra.CalculateTax();
+            //assert
+            Assert.Equal(valorEsperado, imposto);
         }
     }
 }
